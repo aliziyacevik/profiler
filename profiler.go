22abd8c829b162f68282cb0769c4fcb9c9380144
Ali Ziya CEVIK <aliziya.cevik@st.uskudar.edu.tr>
2023-05-02 21:56:18 +0300

    first commit

---
+++ profiler.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port         int
+	OutFile      string
+	OutFormat    string
+	TargetServer string
+}
+
+type RequestInfo struct {
+	Method   string            `json:"method"`
+	URL      string            `json:"url"`
+	Browser  string            `json:"browser"`
+	Header   map[string]string `json:"header"`
+	IP       string            `json:"ip"`
+	Received string            `json:"received"`
+}
+
+func loadConfig(configFile string) (Config, error) {
+	err := godotenv.Load(configFile)
+	if err != nil {
+		return Config{}, err
+	}
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		return Config{}, err
+	}
+
+	return Config{
+		Port:      port,
+		OutFile:   os.Getenv("OUT_FILE"),
+		OutFormat: os.Getenv("OUT_FORMAT"),
+	}, nil
+}
+
+func writeRequestInfoToFile(requestInfo *RequestInfo, fileName string) error {
+	data, err := json.MarshalIndent(requestInfo, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fileName, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getBrowser(userAgent string) string {
+	browserList := []string{"Chrome", "Firefox", "Safari", "Opera", "Edge", "MSIE", "Trident"}
+
+	for _, browser := range browserList {
+		if strings.Contains(userAgent, browser) {
+			return browser
+		}
+	}
+
+	return "Unknown"
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./profiler [profiler.config]")
+		os.Exit(1)
+	}
+
+	config, err := loadConfig(os.Args[1])
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	startProfilerServer(config)
+}
+func startProfilerServer(config Config) {
+	targetURL, err := url.Parse(config.TargetServer)
+	if err != nil {
+		log.Fatalf("Failed to parse target server URL: %v", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		headers := make(map[string]string)
+		for k, v := range r.Header {
+			headers[k] = strings.Join(v, ",")
+		}
+
+		ip := r.RemoteAddr
+		browser := getBrowser(r.Header.Get("User-Agent"))
+		received := time.Now().Format("01-02-06 - 15:04")
+
+		requestInfo := RequestInfo{
+			Method:   r.Method,
+			URL:      r.URL.String(),
+			Header:   headers,
+			IP:       ip,
+			Browser:  browser,
+			Received: received,
+		}
+
+		err := writeRequestInfoToFile(&requestInfo, config.OutFile)
+		if err != nil {
+			log.Printf("Failed to write request info to file: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf("Request info saved to %s", config.OutFile)
+
+		proxy.ServeHTTP(w, r)
+
+	})
+
+	address := fmt.Sprintf(":%d", config.Port)
+	log.Printf("Listening on %s", address)
+	log.Fatal(http.ListenAndServe(address, nil))
+
+}
---
+++ profiler_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+)
+
+// userAppHandler simulates the user's app running on localhost:50133.
+func userAppHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello from user's app!"))
+}
+
+func TestProfiler(t *testing.T) {
+	// Set up the user's app server.
+	userAppServer := httptest.NewServer(http.HandlerFunc(userAppHandler))
+	defer userAppServer.Close()
+
+	// Set up the Profiler configuration.
+	config := Config{
+		Port:         50132,
+		OutFile:      "test_profile.json",
+		OutFormat:    "JSON",
+		TargetServer: userAppServer.URL,
+	}
+
+	// Remove the test output file after the test is done.
+	//	defer os.Remove(config.OutFile)
+
+	// Start the Profiler server.
+	go func() {
+		startProfilerServer(config)
+	}()
+
+	// Wait for the Profiler server to start.
+	time.Sleep(1 * time.Second)
+
+	// Send a request to the Profiler server.
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/test", config.Port))
+	if err != nil {
+		t.Fatalf("Failed to send request to Profiler server: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check if the Profiler server successfully proxies the request to the user's app server.
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Failed to read response body: %v", err)
+	}
+
+	expectedBody := "Hello from user's app!"
+	if string(body) != expectedBody {
+		t.Errorf("Expected response body: %s, got: %s", expectedBody, string(body))
+	}
+
+	// Check if the Profiler server saves the request information to the output file.
+	_, err = os.Stat(config.OutFile)
+	if os.IsNotExist(err) {
+		t.Errorf("Output file not created: %s", config.OutFile)
+	}
+}
